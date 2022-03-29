ae84dd6dd35a5c993c4c75ae02cfce4b3eeb01ac
edwards158 <12884639+edwards158@users.noreply.github.com>
2022-03-29 14:45:43 +0100

    first commit

---
+++ 1-infrastructure/outputs.tf
@@ -0,0 +1,32 @@
+output "vpc_id" {
+  value = aws_vpc.production-vpc.id
+}
+output "vpc_cidr_block" {
+  value = aws_vpc.production-vpc.cidr_block
+}
+output "public_subnet_1_cidr" {
+  value = aws_subnet.public-subnet-1.id
+}
+output "public_subnet_2_cidr" {
+  value = aws_subnet.public-subnet-2.id
+}
+output "public_subnet_3_cidr" {
+  value = aws_subnet.public-subnet-3.id
+}
+output "private_subnet_1_cidr" {
+  value = aws_subnet.private-subnet-1.id
+}
+output "private_subnet_2_cidr" {
+  value = aws_subnet.private-subnet-2.id
+}
+output "private_subnet_3_cidr" {
+  value = aws_subnet.private-subnet-3.id
+}
+output "private_subnets" {
+  value = tolist([aws_subnet.private-subnet-1.id, aws_subnet.private-subnet-2.id, aws_subnet.private-subnet-3.id])
+}
+
+output "public_subnets" {
+  value = tolist([aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id, aws_subnet.public-subnet-3.id])
+}
+
